fa7b0d4d73f8c8b90b4095b5f0dc16531743cef4
a0937582037-gif <a0937582037@gmail.com>
2026-04-09 11:52:32 +0800

    0409

--- web.py
+++ web.py
@@ -1,5 +1,24 @@
 from flask import Flask,render_template, request
 from datetime import datetime
+
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +30,22 @@ def index():
     link += "<a href=/account>Poste傳值</a><hr>"
     link += "<a href=/about>關於我</a><hr>"
     link += "<a href=/calc>次方與根號計算</a><hr>"
+    link += "<br><a href=/read>讀取Firestore資料</a><br>"
+
     return link
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("資管2B 2026")    
+    #docs = collection_ref.get()
+    docs = collection_ref.order_by("lab",direction=firestore.Query.DESCENDING).stream()
+    #data_list.sort(key=sort_logic)
+    for doc in docs:         
+        Result += str(doc.to_dict()) + "<br>"    
+    return Result
+
 @app.route("/mis")
 def course():
     return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
